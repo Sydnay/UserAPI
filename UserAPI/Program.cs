aec820337b92995c7d0452a057a3bf738e543bd5
Me <Pyko_Horyc@mail.ru>
2022-05-04 23:38:22 +0400

    Docker should work but not sure

--- UserAPI/Program.cs
+++ UserAPI/Program.cs
@@ -26,9 +26,9 @@ if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
-}
 
-app.UseHttpsRedirection();
+    app.UseHttpsRedirection();
+}
 
 app.UseAuthorization();
 
